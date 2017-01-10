df54947d2e26b721eb0354bdf1d075a8523f88fc
ouassimos9 <akebli_ouassim@hotmail.com>
2017-01-10 10:37:17 +0100

    code affichage.c 2 version

---
+++ code/V1/main.c
@@ -0,0 +1,22 @@
+// Date : 06/01/2017
+// Auteurs : JACQUIEZ Romain, Ouassim AKEBLI et Antoine BERENGUER
+// Contenu : le module main.c
+// Resume : le module qui reprend toutes les fonctons pour créer le programme
+
+#include <stdlib.h>
+#include "GfxLib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "affichage.h"
+
+int main()
+{
+	static Grille Plateau;
+	
+	Initialisation_Grille(Plateau);
+	
+	Affiche_Grille(Plateau);	
+	
+	return(0);
+}
